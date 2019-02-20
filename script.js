ab8ad06c4e5d7d148675abb7ff863740b9654242
Scott Wells <efwells101@gmail.com>
2019-02-20 13:56:46 -0700

    change script for button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+window.onload = function() {
+	clickerButton();
+};
+
 //1. Find and store the element we want to listen to events on.
 
-const textChangeColor = document.getElementById("p1");
+const clickerButton = document.getElementById("p1");
 
 //2. Define the function that will respond to the event.
 
-const onButtonClick = () => {
-	textChangeColor.body.style.color = "deeppink";
-	console.log(textChangeColor);
-}
+const onButtonClick = function() {
+	clickerButton.textContent = "Oh snap! Where'd the paragraph go?";
+};
 
 //3. Add the event listener for the element and function.
 
