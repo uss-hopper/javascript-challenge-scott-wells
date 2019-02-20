a4beae529bb9a02a753ec5e304254a0c0fff97dc
Scott Wells <efwells101@gmail.com>
2019-02-20 12:04:11 -0700

    add button click event that changes text color

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//on click event listener
+//1. Find and store the element we want to listen to events on.
+
+const textChangeColor = document.getElementById("p1");
+
+//2. Define the function that will respond to the event.
+
+const onButtonClick = () => {
+	textChangeColor.body.style.color = "deeppink";
+	console.log(textChangeColor);
+}
+
+//3. Add the event listener for the element and function.
+
+textChangeColor.addEventListener("click", onButtonClick);
