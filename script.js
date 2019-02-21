b857d4486edfe244cc647df4e494e493a28f5e6c
Scott Wells <efwells101@gmail.com>
2019-02-20 22:29:27 -0700

    add button 3 and button 4, trying to get code to work

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-window.onload = function() {
-	clickerButton();
-};
 
-//1. Find and store the element we want to listen to events on.
-
-const clickerButton = document.getElementById("p1");
-
-//2. Define the function that will respond to the event.
-
-const onButtonClick = function() {
-	clickerButton.textContent = "Oh snap! Where'd the paragraph go?";
-};
-
-//3. Add the event listener for the element and function.
-
-textChangeColor.addEventListener("click", onButtonClick);
+const badWordsButton = document.getElementById("b-03");
+const changeWords = function() {
+	const words = document.getElementById("p2");
+	const array = words.toLowerCase().split(" ");
+	const newParagraph = array.filter(index => index.contains("I love"));
+	console.log(newParagraph);
+}
+badWordsButton.addEventListener("click", changeWords);
